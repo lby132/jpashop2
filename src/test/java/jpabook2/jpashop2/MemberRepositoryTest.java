afab3aa43524658836638c2758f83f7f489fa53b
lby132 <quddbscjswow@naver.com>
2022-11-23 01:46:01 +0900

    jpa활용1복습

---
+++ src/test/java/jpabook2/jpashop2/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package jpabook2.jpashop2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    //@Rollback(value = false)
+    void testMember() throws Exception {
+        final Member member = new Member();
+        member.setUsername("memberA");
+
+        final Long saveId = memberRepository.save(member);
+        final Member findMember = memberRepository.find(saveId);
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+
+    }
+}
