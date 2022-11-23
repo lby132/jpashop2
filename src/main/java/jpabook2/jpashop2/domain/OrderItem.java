d16fe124a3ee9275ceb3faac9b2865483c435a6d
lby132 <quddbscjswow@naver.com>
2022-11-24 01:59:18 +0900

    엔티티 클래스 세팅 연관관계 설정.

---
+++ src/main/java/jpabook2/jpashop2/domain/OrderItem.java
@@ -0,0 +1,29 @@
+package jpabook2.jpashop2.domain;
+
+import jpabook2.jpashop2.domain.item.Item;
+import jpabook2.jpashop2.domain.item.Order;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice;
+    private int count;
+}
---
+++ src/main/java/jpabook2/jpashop2/domain/item/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package jpabook2.jpashop2.domain.item;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
